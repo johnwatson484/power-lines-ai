b642770e773f30fac5d37cb59107944cdc44a7a8
John Watson <john.watson@defra.gov.uk>
2024-07-25 21:07:44 +0100

    Add initial model and data

---
+++ PowerLinesAI/PowerLinesAI/Program.cs
@@ -0,0 +1,21 @@
+﻿//Load sample data
+using PowerLinesAI;
+
+var sampleData = new ResultModel.ModelInput()
+{
+    Division = @"E0",
+    HomeTeam = @"Aston Villa",
+    AwayTeam = @"QPR",
+};
+
+//Load model and predict output
+var result = ResultModel.Predict(sampleData);
+
+var resultDictionary = new Dictionary<string, string>()
+{
+    { "H", "Home win" },
+    { "A", "Away win" },
+    { "D", "Draw" }
+};
+
+Console.WriteLine($"Predicted result for {sampleData.HomeTeam} v {sampleData.AwayTeam} is {resultDictionary[result.PredictedLabel]}");
